25c53e47304c49ea8eeea5a9aa3a1fb19619fb94
Lukeismugetha <luke.wabuiya@strathmore.edu>
2022-05-05 15:55:52 +0300

    Header completed

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('footer');
+    
+        echo view('header');
+        echo view('footer');
     }
 }
---
+++ app/Views/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="bg-secondary">
+  <div class="max-w-full mx-auto px-4 sm:px-6">
+    <div class="flex justify-between items-center h-16 md:justify-start md:space-x-10">
+      <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="/">
+                    <img class="h-8 w-auto sm:h-10" src="https://www.co-opbank.co.ke/wp-content/uploads/2021/11/coop-bank-logo.png" alt="Coop Bank Logo">
+        </a>
+      </div>
+      <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+        <a href="https://onlinebanking.co-opbank.co.ke/" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-900 hover:bg-green-700">
+          <svg class="h-4 w-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokelinecap="round" strokelinejoin="round" strokewidth="{2}" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+          </svg>
+          Sign in to Internet Banking
+        </a>
+        <a href="https://www.co-opbank.co.ke/search">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 ml-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+          </svg>
+        </a>
+      </div>
+
+      <div class="-mr-2 -my-2 md:hidden">
+        <button type="button" class="btn-open first bg-secondary rounded-md p-2 inline-flex items-center justify-center text-white hover:text-black hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500" aria-expanded="false">
+          <span class="sr-only">Open menu</span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
