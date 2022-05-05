25307a1b81da0ad9fefdd7341a95fb2ff957057e
Lukeismugetha <luke.wabuiya@strathmore.edu>
2022-05-05 11:05:59 +0300

    footer edit

---
+++ app/Views/footer.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<footer class="bg-white" aria-labelledby="footerHeading">
+  <h2 id="footerHeading" class="sr-only">Footer</h2>
+  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-10 lg:px-8">
+    <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+      <div class="grid grid-cols-2 gap-8 xl:col-span-2">
+        <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div>
+            <h3 class="text-sm font-semibold text-primary tracking-wider uppercase">
+              About Us
+            </h3>
+
+						<div class="mt-4 space-y-1"><ul id="menu-about-us" class=""><li id="menu-item-715" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-715 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/about-us/">Who We Are</a></li>
+<li id="menu-item-16656" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16656 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/management/">Our Management</a></li>
+<li id="menu-item-714" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-714 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/our-history/">Our History</a></li>
+<li id="menu-item-713" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-713 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/our-awards/">Our Awards</a></li>
+<li id="menu-item-712" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-712 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/certification/">Certification</a></li>
+<li id="menu-item-1703" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1703 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/sustainability/">Sustainability</a></li>
+<li id="menu-item-47681" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47681 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/faqs/">FAQs</a></li>
+</ul></div>          </div>
+
+          <div class="mt-12 md:mt-0">
+            <h3 class="text-sm font-semibold text-primary tracking-wider uppercase">
+              Quick Links
+            </h3>
+            <div class="mt-4 space-y-1"><ul id="menu-quick-links" class=""><li id="menu-item-788" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-788 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/careers/">Careers</a></li>
+<li id="menu-item-789" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-789 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/wp-content/uploads/2021/12/Co_op_Bank_Tariff_Guide10_09_21_03540e492c.pdf">Tariff Guides</a></li>
+<li id="menu-item-790" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-790 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/wp-content/uploads/2022/04/School-Business-Codes.pdf">School Business Codes</a></li>
+<li id="menu-item-793" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-793 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/application-forms/">Application Forms</a></li>
+<li id="menu-item-802" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-802 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/branch-contacts-operating-hours/">Branch Contacts &amp; Operating Hours</a></li>
+<li id="menu-item-801" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-801 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/agent-locator/">Agent Locator</a></li>
+<li id="menu-item-1340" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1340 text-base text-black hover:text-secondary"><a href="https://campaign.co-opbank.co.ke/back-to-school/">Back to School</a></li>
+<li id="menu-item-16674" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16674 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/partnerships/">Partnerships and Discounts</a></li>
+<li id="menu-item-1706" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1706 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/contact-us/">Contact Us</a></li>
+</ul></div>          </div>
+
+        </div>
+        <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div>
+            <h3 class="text-sm font-semibold text-primary tracking-wider uppercase">
+              Related Sites
+            </h3>
+            <div class="mt-4 space-y-1"><ul id="menu-related-sites" class=""><li id="menu-item-776" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-776 text-base text-black hover:text-secondary"><a href="https://diaspora.co-opbank.co.ke">Diaspora Banking</a></li>
+<li id="menu-item-777" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-777 text-base text-black hover:text-secondary"><a href="https://msme.co-opbank.co.ke/">MSME Portal</a></li>
+<li id="menu-item-778" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-778 text-base text-black hover:text-secondary"><a href="https://goodhome.co.ke/">Good Home Portal</a></li>
+<li id="menu-item-779" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-779 text-base text-black hover:text-secondary"><a href="https://vehiclesales.co-opbank.co.ke/">Vehicles for Sale Portal</a></li>
+<li id="menu-item-780" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-780 text-base text-black hover:text-secondary"><a href="https://insurance.co-opbank.co.ke/">Bancassurance</a></li>
+<li id="menu-item-781" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-781 text-base text-black hover:text-secondary"><a href="https://developer.co-opbank.co.ke:9443/devportal/">Coop APIs Portal</a></li>
+<li id="menu-item-782" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-782 text-base text-black hover:text-secondary"><a href="https://foundation.co-opbank.co.ke/">Co-op Foundation</a></li>
+<li id="menu-item-783" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-783 text-base text-black hover:text-secondary"><a href="https://co-opbankss.com/">South Sudan</a></li>
+<li id="menu-item-784" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-784 text-base text-black hover:text-secondary"><a href="https://kingdomsecurities.co.ke/">Kingdom Securities</a></li>
+<li id="menu-item-16671" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-16671 text-base text-black hover:text-secondary"><a href="https://suppliers.co-opbank.co.ke:44300/slc_selfreg/">Supplier Registration</a></li>
+<li id="menu-item-785" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-785 text-base text-black hover:text-secondary"><a href="https://sacco-onlinebanking.co-opbank.co.ke/iportalweb/iRetail@1">Sacco MCo-opCash</a></li>
+</ul></div>          </div>
+          <div class="mt-12 md:mt-0">
+            <h3 class="text-sm font-semibold text-primary tracking-wider uppercase">
+                Policies
+            </h3>
+            <div class="mt-4 space-y-1"><ul id="menu-policies" class=""><li id="menu-item-811" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-811 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/disclaimer/">Disclaimer</a></li>
+<li id="menu-item-810" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-810 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/terms-and-conditions/">Terms and Conditions</a></li>
+<li id="menu-item-809" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-809 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/privacy-policy-2/">Privacy Policy</a></li>
+<li id="menu-item-1346" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1346 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/wp-content/uploads/2021/12/Complaints_Resolution_Guide_A2_Poster_6083c1455a.pdf">Complaints Resolution Guide</a></li>
+<li id="menu-item-16915" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16915 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/whistleblowing/">Whistleblowing</a></li>
+<li id="menu-item-16916" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16916 text-base text-black hover:text-secondary"><a href="https://www.co-opbank.co.ke/business-code-of-conduct-and-ethics/">Business Code of Conduct and Ethics</a></li>
+</ul></div>          </div>
+        </div>
+      </div>
+      <div class="mt-8 xl:mt-0">
+        <h3 class="text-sm font-semibold text-primary tracking-wider uppercase">
+          Contact Us
+        </h3>
+        <p class="mt-4 text-base text-black">
+          <span>If you have any feedback or complaint, please talk to us on:</span> <br>
+          <span>Call Center Numbers:</span> <a href="tel:020-2776000" class="text-primary">020-2776000,</a> <a href="tel:0703027000" class="text-primary">0703027000</a> <br>
+          <span>Email:</span> <a href="mailto:customerservice@co-opbank.co.ke" class="text-primary">customerservice@co-opbank.co.ke</a><br>
+          <span>Whatsapp:</span> <a href="https://wa.me/254736690101" class="text-primary">+254736690101</a>
+        </p>
+
+        <div class="flex py-4 space-x-4 md:order-2">
+          <a target="_blank" href="https://www.facebook.com/coopbankenya/" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Facebook</span>
+            <svg class="h-6 w-6" fill="#1877f2" viewBox="0 0 24 24" aria-hidden="true">
+              <path fillrule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" cliprule="evenodd"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://www.instagram.com/coopbankenya/?hl=en" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Instagram</span>
+            <svg class="h-6 w-6" fill="#c32aa3" viewBox="0 0 24 24" aria-hidden="true">
+              <path fillrule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" cliprule="evenodd"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://twitter.com/Coopbankenya" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Twitter</span>
+            <svg class="h-6 w-6" fill="#1da1f2" viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://www.linkedin.com/company/62404/" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Linkdin</span>
+            <svg class="h-6 w-6" fill="#0a66c2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://www.youtube.com/co-opbankkenya" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Youtube</span>
+            <svg class="h-6 w-6" fill="#ff0000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://wa.me/254736690101" class="text-gray-400 hover:text-black">
+            <span class="sr-only">WhatsApp</span>
+            <svg class="h-6 w-6" fill="#25d366" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://www.tiktok.com/@coopbankenya" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Tiktok</span>
+            <svg class="h-6 w-6" viewBox="-58.35000000000002 -186.70564362582354 2548.289756960746 2538.849821747569" xmlns="http://www.w3.org/2000/svg">
+              <g fill="#25f4ee">
+                <path d="M779.38 890.55v-88.12a650.81 650.81 0 0 0-92.45-7.94c-299.8-.64-565.22 193.64-655.25 479.6S47.92 1871.34 294 2042.56a684.7 684.7 0 0 1 485.36-1152z"></path>
+                <path d="M796 1888.72c167.62-.23 305.4-132.28 312.74-299.74V94.62h273A512.17 512.17 0 0 1 1373.8 0h-373.41v1492.92c-6.21 168.31-144.32 301.63-312.74 301.9a317.76 317.76 0 0 1-144.45-36.11A313.48 313.48 0 0 0 796 1888.72zM1891.66 601.64v-83.06a509.85 509.85 0 0 1-282.4-85.22 517.79 517.79 0 0 0 282.4 168.28z"></path>
+              </g>
+              <path d="M1609.26 433.36a514.19 514.19 0 0 1-127.84-339.47h-99.68a517.16 517.16 0 0 0 227.52 339.47zM686.93 1167.9a313.46 313.46 0 0 0-144.46 590.81A312.75 312.75 0 0 1 796 1262.51a329.69 329.69 0 0 1 92.44 14.49V897.05a654.77 654.77 0 0 0-92.44-7.22h-16.62v288.9a321.13 321.13 0 0 0-92.45-10.83z" fill="#fe2c55"></path>
+              <path d="M1891.66 601.64v288.91a886.23 886.23 0 0 1-517.86-168.29v759.1c-.8 378.78-308.09 685.43-686.87 685.43A679.65 679.65 0 0 1 294 2042.56 685.43 685.43 0 0 0 1481.42 1576V819.05A887.71 887.71 0 0 0 2000 985.17v-372a529.59 529.59 0 0 1-108.34-11.53z" fill="#fe2c55"></path>
+              <path d="M1373.8 1481.36v-759.1a886.11 886.11 0 0 0 518.58 166.12v-288.9a517.87 517.87 0 0 1-283.12-166.12 517.16 517.16 0 0 1-227.52-339.47h-273V1589a313.46 313.46 0 0 1-567 171.17 313.46 313.46 0 0 1 144.46-590.83 321.35 321.35 0 0 1 92.45 14.45V894.88A684.71 684.71 0 0 0 293.29 2050.5a679.65 679.65 0 0 0 393.64 116.29c378.78 0 686.07-306.65 686.87-685.43z"></path>
+            </svg>
+          </a>
+
+          <a target="_blank" href="https://www.flickr.com/photos/147086508@N03" class="text-gray-400 hover:text-black">
+            <span class="sr-only">Flickr</span>
+            <svg class="h-6 w-6" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsxlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 362.68 362.68" style="{{" enablebackground:="" 'new="" 0="" 362.68="" 362.68'="" }}="" xmlspace="preserve">
+              <circle style="{{" fill:="" '#005de1'="" }}="" cx="85.8" cy="181.34" r="85.8"></circle>
+              <circle style="{{" fill:="" '#ff007e'="" }}="" cx="276.88" cy="181.34" r="85.8"></circle>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+            </svg>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="max-w-full p-8 bg-primary mx-auto">
+    <div class="md:flex md:items-center md:justify-between">
+      <p class="mt-8 text-xs text-white md:mt-0 md:order-1">
+        Authorized financial services and registered credit provider. Co-operative Bank is Regulated by the Central Bank of Kenya.
+      </p>
+      <p class="mt-8 text-xs text-white md:mt-0 md:order-1">
+        © 2022 Co-operative Bank Kenya All Rights Reserved
+      </p>
+    </div>
+  </div>
+</footer>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
